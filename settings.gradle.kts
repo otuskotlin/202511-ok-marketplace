af1360e4aac55e953ac13b8f8e555be73a57952f
Sergey Okatov <sokatov@gmail.com>
2025-02-27 22:55:42 +0500

    M1l1 first app and test
    (cherry picked from commit 11c49a70172434b712fc353ef90d18863a205af6)
    (cherry picked from commit 5313e5d6de9b32b4ce96254ad762b44485218d68)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
-rootProject.name = "otuskotlin-marketplace-202411"
+rootProject.name = "otuskotlin-marketplace-202505"
 
 include("m1l1-first")
 
