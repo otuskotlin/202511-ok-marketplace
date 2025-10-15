ac684115febaf5c39d89a84b99ff6a665befd3ba
Сальников Евгений Владимирович <salnikov_e_v@m00office0207.local>
2025-10-15 21:58:22 +0300

    M3L5-devops

--- ok-marketplace-be/ok-marketplace-tmp/build.gradle.kts
+++ ok-marketplace-be/ok-marketplace-tmp/build.gradle.kts
@@ -1,3 +1,26 @@
 plugins {
     id("build-jvm")
 }
+
+repositories {
+    maven {
+        name = "LocalRepo"
+        url = uri("${rootProject.projectDir}/../ok-marketplace-other/build/repo")
+    }
+}
+
+val resourcesFromLib by configurations.creating
+
+dependencies {
+    resourcesFromLib("ru.otus.otuskotlin.marketplace:dcompose:1.0:resources@zip")
+}
+
+tasks.register<Copy>("extractLibResources") {
+    from(zipTree(resourcesFromLib.singleFile))
+    into(layout.buildDirectory.dir("dcompose"))
+}
+
+tasks["build"].dependsOn("extractLibResources")
+
+
+
