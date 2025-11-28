cd8f38514ab69f69000f076d7365f8960b5c3269
Sergey Okatov <sokatov@gmail.com>
2025-11-28 11:30:40 +0500

    M1L1 First App

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,9 +6,9 @@ pluginManagement {
 }
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
-rootProject.name = "otuskotlin-marketplace-202508"
+rootProject.name = "otuskotlin-marketplace-202511"
 
 include("m1l1-first")
 
