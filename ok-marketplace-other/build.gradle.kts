994907b913890d36e2c759ebeeeaf024ef7c15de
Sergey Okatov <sokatov@gmail.com>
2025-12-24 20:52:10 +0300

    M7l4 PG (fix migrations task names)

--- ok-marketplace-other/build.gradle.kts
+++ ok-marketplace-other/build.gradle.kts
@@ -28,5 +28,7 @@ tasks {
     register("buildInfra") {
         group = "build"
         dependsOn(project(":ok-marketplace-dcompose").getTasksByName("publish",false))
+        dependsOn(project(":ok-marketplace-migration-cs").getTasksByName("buildImages",false))
+        dependsOn(project(":ok-marketplace-migration-pg").getTasksByName("buildImages",false))
     }
 }
