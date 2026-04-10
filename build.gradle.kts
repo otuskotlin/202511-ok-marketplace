d17c69256b7f4cfaa45c4713dac374a08c781384
Сальников Евгений Владимирович <salnikov_e_v@m00office0207.local>
2026-04-10 13:40:54 +0300

    build back

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,7 +44,6 @@ tasks {
     register("check") {
         group = "verification"
         dependsOn(gradle.includedBuild("ok-marketplace-be").task(":check"))
-        dependsOn("e2eTests")
     }
 }
 
